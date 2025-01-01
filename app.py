256f8b4d22f521df2045d10663524564282a8870
Bilgehan Ceylan <Ceylanab@gmail.com>
2025-01-01 14:07:07 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -42,4 +42,6 @@ def home():
     return "YouTube Video Downloader API is running."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render'ın PORT değişkenini al
+    app.run(host='0.0.0.0', port=port)       # 0.0.0.0 tüm IP'lerden erişim sağlar
+
